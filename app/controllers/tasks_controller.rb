9d527e50adde53155bd564390dca41d9e4f22ba9
miyatani <ec2-user@ip-172-31-5-122.us-east-2.compute.internal>
2018-01-31 10:04:21 +0000

    taskController#update#delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,6 +27,26 @@ end
     @task = Task.find(params[:id])
   end
   
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = 'タスクを編集しました'
+      redirect_to @task
+    else
+      flash.now[:danger] = "タスクを編集できませんでした"
+      render :edit
+  end
+end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = "タスクは削除されました"
+    redirect_to tasks_url
+  end
+  
   private
   
   # Strong Parameter
